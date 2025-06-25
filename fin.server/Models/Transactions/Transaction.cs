6580cb8136636312917f71602fc51cc0a097e239
meir <meirevenhaim018@gmail.com>
2025-06-26 00:27:12 +0300

    init

---
+++ fin.server/Models/Transactions/Transaction.cs
@@ -0,0 +1,13 @@
+﻿namespace fin.server.Models.Transactions
+{
+    public class Transaction
+    {
+        public int TransactionId { get; set; } 
+        public decimal Amount { get; set; }
+
+        public TransactionType Type { get; set; }
+
+        public string? Note { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ fin.server/Repository/TransactionRepo.cs
@@ -0,0 +1,26 @@
+﻿using fin.server.Data;
+using fin.server.Models.Transactions;
+using fin.server.Repository.Interface;
+
+namespace fin.server.Repository
+{
+    public class TransactionRepo : ITransaction
+    {
+        private readonly FinContext _context;
+        public TransactionRepo(FinContext context)
+        {
+            _context = context;
+        }
+        public async Task<bool> IsCreditCardExist(int creditNumber, int cvc, DateTime creditDate)
+        {
+            Transaction? transaction = await _context.Transactions.FindAsync(creditNumber, cvc, creditDate);
+            return transaction != null;
+        }
+
+        public async Task<bool> IsCreditCardExist(int creditNumber, int cvc)
+        {
+            Transaction? transaction = await _context.Transactions.FindAsync(creditNumber, cvc);
+            return transaction != null;
+        }
+    }
+}
